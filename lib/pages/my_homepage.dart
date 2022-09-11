9c5b9904645cbc2c46cfe1c0f89254bba5e53adb
Reagandennis <dennisowiti215@gmail.com>
2022-09-11 12:15:36 +0300

    new

---
+++ lib/pages/my_homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
