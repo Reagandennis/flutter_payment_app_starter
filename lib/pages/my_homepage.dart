29882aea357ab9ad8e4e3fed3bcdaa97126e2e37
Reagandennis <dennisowiti215@gmail.com>
2022-09-13 10:31:59 +0300

    first stage

--- lib/pages/my_homepage.dart
+++ lib/pages/my_homepage.dart
@@ -12,9 +12,49 @@ class MyHomepage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomepage> {
   @override
   Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: AppColor.backGroundColor,
-      body: Container(),
+      body: Container(
+        height: h,
+        child: Stack(
+          children: [
+            _headSection(),
+          ],
+        ),
+      ),
     );
+  }
+
+  _headSection() {
+    return Container(
+      height: 310,
+      child: Stack(
+        children: [
+          _mainBackground(),
+          _curveImageContainer(),
+        ],
+      ),
+    );
+  }
+
+  _mainBackground() {
+    return Positioned(
+        child: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage("images/background.png"))),
+    ));
+  }
+
+  _curveImageContainer() {
+    return Positioned(
+        child: Container(
+            height: MediaQuery.of(context).size.height * 0.1,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("images/curve.png")))));
   }
 }
