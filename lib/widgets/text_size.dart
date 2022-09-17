51564006456f118b9fcecad13f23ecc8ec5fdced
Reagandennis <dennisowiti215@gmail.com>
2022-09-17 15:51:16 +0300

    new

---
+++ lib/widgets/text_size.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SizedText extends StatelessWidget {
+  const SizedText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
